bf863a457acda36b436ddbd72c9748db64731596
lfoster1150 <lfoster1150@gmail.com>
2021-09-20 19:03:36 -0500

    Added Files to react cllient folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import './App.css'
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
+
   return (
     <div className="App">
       <h1>APP</h1>
