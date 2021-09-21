1c855e339fbfd310be2eec3defcbf5010345aa02
lfoster1150 <lfoster1150@gmail.com>
2021-09-21 10:49:44 -0500

    Login working

---
+++ client/src/components/ProtectedRoute.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Redirect, Route } from 'react-router'
+
+export default function ProtectedRoute({
+  user,
+  authenticated,
+  component: Component,
+  ...rest // allows us to provide the remaining props defined to the route below.
+}) {
+  // Accepts user and authenticated as props to check if the current user is authorized
+
+  // Component is the component we want to render if the user is logged in and authenticated
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        user && authenticated ? (
+          <Component {...props} /> // Render our chosen component if a user exists and they are authenticated
+        ) : (
+          <Redirect to="/login" /> // Otherwise, use the Redirect component to return the user to the sign in screen
+        )
+      }
+    />
+  )
+}
---
+++ client/src/pages/User.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+
+const User = () => {
+  return (
+    <div>
+      <h1>User Page</h1>
+    </div>
+  )
+}
+
+export default User
