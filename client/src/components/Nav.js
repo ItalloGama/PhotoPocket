5cbbb9699fede87785d43f750c91b52907f36c73
Janay Anguiano <janayanguiano@gmail.com>
2021-09-23 13:08:24 -0700

    home page logo

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { Navbar, Nav, Container } from 'react-bootstrap'
-import logo from '../screenshot/logo.png'
+import logo from '../screenshot/logo2.png'
 
 export default function Navigation({ authenticated, user, handleLogOut }) {
   let authenticatedOptions
@@ -17,7 +17,14 @@ export default function Navigation({ authenticated, user, handleLogOut }) {
     <Navbar variant="dark" className="nav-bar">
       <Container>
         <Navbar.Brand href="/">
-          <img src={logo} margin-right="30" alt="" />
+          <img
+            src={logo}
+            width="120"
+            height="30"
+            margin-right="30"
+            alt=""
+            margin-top="20"
+          />
         </Navbar.Brand>
         <Nav className="me-auto">
           {authenticated && user ? authenticatedOptions : publicOptions}
