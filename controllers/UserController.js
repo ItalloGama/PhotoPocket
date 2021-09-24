5dee33fc5ad0276f067b2bf1bd82e46716d208e7
lfoster1150 <lfoster1150@gmail.com>
2021-09-23 21:01:56 -0500

    Fixed issue with user update

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,17 @@
 const { User } = require('../models')
 
 const UpdateUser = async (req, res) => {
-  console.log(req.body)
+  let data = req.body
+  let newData = {}
+  for (let key in data) {
+    if (data[key]) {
+      newData = { ...newData, key: data[key] }
+    }
+  }
   try {
+    console.log(req.body)
     const user = await User.update(
-      { ...req.body },
+      { ...newData },
       { where: { id: req.params.user_id }, returning: true }
     )
     res.send(user)
